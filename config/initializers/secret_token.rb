9a226bc1c7654e203b9c62f4131063bdca8b3d8e
Weston Novelli <Weston.Novelli@Blackbaud.com>
2014-07-24 17:41:25 -0500

    all the rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Turndownforlunch::Application.config.secret_key_base = '8fae9416ad242cd11e9705f920d2bc33b7168ee103906d46535bdb62763e3cec003d1979388a9e1d06c64029da7af07d899f2712ecb156b6b52ce1ef4d2adf57'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Turndownforlunch::Application.config.session_store :cookie_store, key: '_turndownforlunch_session'
