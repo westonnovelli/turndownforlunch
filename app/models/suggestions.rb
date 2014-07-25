7f8eaa724fd00ad5bd63d770143fc1109a6934e0
Weston Novelli <Weston.Novelli@Blackbaud.com>
2014-07-24 21:07:03 -0500

    added logging to the users and suggestions models.

--- app/models/suggestions.rb
+++ app/models/suggestions.rb
@@ -1,6 +1,7 @@
 class Suggestions < ActiveRecord::Base
 
 	def vote_up
+		logger.info "Suggestion #{:location}: #{:depature_time} has been up voted!"
 		self.vote += 1
 		save
 	end
--- app/models/users.rb
+++ app/models/users.rb
@@ -2,15 +2,20 @@ class Users < ActiveRecord::Base
 
 	def suggestion
 		self.suggestion
+		logger.info "User #{:name}'s suggestion is #{self.suggestion}."
 		# TODO what happens when the suggestion is nil?
 	end
 
 	def make_suggestion(suggestion)
-		self.suggestion = suggestion
-		save
+		unless self.suggestion != nil
+			self.suggestion = suggestion
+			save
+		end
+		logger.info "User #{:name} tried to vote for more than one suggestion."
 	end
 
 	def undo_suggestion
+		logger.info "User #{:name} is undoing his/her vote."
 		self.suggestion = nil
 		# TODO should we have some other default value, or just handle the null
 		save
