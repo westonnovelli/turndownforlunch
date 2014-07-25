9101f4e82196a68db322b70361391b994404957f
Blackbaud-KyleKurihara <kyle.kurihara@blackbaud.com>
2014-07-25 17:27:33 -0500

    Trying to avoid bootstrap gem.
    Currently it only supports up to bootstrap 2.3.2 - trying to avoid
    confusions since we are using 3.2.0. Same functionality!

--- Gemfile
+++ Gemfile
@@ -32,8 +32,6 @@ group :doc do
   gem 'sdoc', require: false
 end
 
-gem 'twitter-bootstrap-rails'
-
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, :notice => "Logged in!"
     else
-      flash.now.alert = "Invalid name"
+      flash[:alert] = "Invalid name"
       render "new"
     end
   end
