34272f657640a9eda980a4cdec115ec5e58681ae
kuriharak93 <kuriharak93@gmail.com>
2014-07-24 21:36:12 -0500

    Saving names to database.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session[:user] = "#{params[:firstName]} #{params[:lastName]}"
-    redirect_to root_url, :notice => "Logged in!"
+    user = Users.findUser(params[:firstName], params[:lastName])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url, :notice => "Logged in!"
+    else
+      flash.now.alert = "Invalid name"
+      render "new"
+    end
   end
 
   def destroy
-    session[:user] = nil
+    session[:user_id] = nil
     redirect_to root_url, :notice => "Logged out!"
   end
 end
